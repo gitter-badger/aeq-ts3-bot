d8aee3df9909e74a9293ed24bb45eaf1c85a8f97
Glains <lukas-kannenberg@web.de>
2017-07-27 01:18:25 +0200

    refactor(ApplicationAcceptCommand): adjust to new api, implement permissions

--- src/main/java/de/esports/aeq/ts3bot/command/ApplicationAcceptCommand.java
+++ src/main/java/de/esports/aeq/ts3bot/command/ApplicationAcceptCommand.java
@@ -1,22 +1,23 @@
 package de.esports.aeq.ts3bot.command;
 
 import com.beust.jcommander.Parameter;
+import de.esports.aeq.ts3bot.command.api.Command;
+import de.esports.aeq.ts3bot.command.api.CommandPermissions;
+import de.esports.aeq.ts3bot.core.AeQESportsTS3Bot;
+import de.esports.aeq.ts3bot.core.api.UserGroups;
+import de.esports.aeq.ts3bot.handler.ApplicationAcceptHandler;
+import de.esports.aeq.ts3bot.handler.api.MessageHandler;
+import de.esports.aeq.ts3bot.permission.PermissionCheck;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Created by Lukas on 24.07.2017.
  */
 public class ApplicationAcceptCommand implements Command {
 
-    /**
-     * The ts3id of the teamspeak user.
-     */
     @Parameter()
     private String ts3id;
-
-    public ApplicationAcceptCommand(String ts3id) {
-        this.ts3id = ts3id;
-    }
 
     public @NotNull String getTs3id() {
         return ts3id;
@@ -26,4 +27,15 @@ public class ApplicationAcceptCommand implements Command {
         this.ts3id = ts3id;
     }
 
+    @Override
+    public @Nullable MessageHandler createMessageHandler(@NotNull AeQESportsTS3Bot ts3Bot) {
+        return new ApplicationAcceptHandler(ts3Bot);
+    }
+
+    @Override
+    public @Nullable CommandPermissions getPermissions() {
+        return (eventInfo, isFullAdmin, isAdmin) -> {
+            return new PermissionCheck(eventInfo, isFullAdmin, isAdmin).requireUserGroup(UserGroups.MEMBER_RECRUITER).validate();
+        };
+    }
 }
---
+++ src/main/java/de/esports/aeq/ts3bot/core/api/UserGroups.java
@@ -0,0 +1,10 @@
+package de.esports.aeq.ts3bot.core.api;
+
+/**
+ * Created by Lukas on 27.07.2017.
+ */
+public class UserGroups {
+
+    public static final String MEMBER_RECRUITER = "Mitgliederrekrutierer";
+
+}
