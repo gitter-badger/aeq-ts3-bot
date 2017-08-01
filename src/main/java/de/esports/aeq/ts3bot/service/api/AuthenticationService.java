459e47cdd435bda9cf8fd53cf791c81d444ee437
Glains <lukas-kannenberg@web.de>
2017-08-01 16:56:19 +0200

    feat(authentication): add authentication service template

---
+++ src/main/java/de/esports/aeq/ts3bot/service/api/AuthenticationService.java
@@ -0,0 +1,20 @@
+package de.esports.aeq.ts3bot.service.api;
+
+import de.esports.aeq.ts3bot.core.api.Credentials;
+
+/**
+ * Responsible for authenticating users that are persisted in the database.
+ *
+ * @author Lukas Kannenberg
+ * @since 01.07.2017
+ */
+public interface AuthenticationService {
+
+    /**
+     * Authenticates a user with the given {@link Credentials}.
+     *
+     * @param credentials the {@link Credentials} of the user
+     * @return true if the authentication is successful, otherwise false
+     */
+    boolean authenticateUser(Credentials credentials);
+}
